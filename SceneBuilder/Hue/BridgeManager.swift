ec0bb38ee122d53b34c8eaf0d7be9bb2dd9080ae
Cory Wilhite <cory.wilhite@gmail.com>
2016-12-06 23:00:10 -0600

    Successfully waiting 30 seconds for the link button to be pressed in order to connect with the hue bridge. Successfully retrieves a new username.

--- SceneBuilder/Hue/BridgeManager.swift
+++ SceneBuilder/Hue/BridgeManager.swift
@@ -31,13 +31,15 @@ class BridgeManager {
     
     func startBridgeRegistration(for configuration: Configuration) {
         
-        Timer.scheduledTimer(withTimeInterval: 3, repeats: true) { timer in
+        Timer.scheduledTimer(withTimeInterval: 5, repeats: true) { timer in
             
             self.requestBridgePermssion(for: configuration, timer: timer)
             
         }
         
     }
+    
+    var tries = 0
     
     func requestBridgePermssion(for configuration: Configuration, timer: Timer) {
         
@@ -53,7 +55,35 @@ class BridgeManager {
             .responseJSON { (response) in
                 print(response)
                 
-                timer.invalidate()
+                guard let rawResponse = response.result.value as? [[String: Any]],
+                    let responseDictionary = rawResponse.first else {
+                        print("unexpected response")
+                        timer.invalidate()
+                        return
+                }
+                
+                if let successDictionary = responseDictionary["success"] as? [String: Any],
+                    let username = successDictionary["username"] as? String {
+                    
+                    print("Retrieved Username: \(username)")
+                    timer.invalidate()
+                    return
+                    
+                } else if let errorDictionary = responseDictionary["error"] as? [String: Any],
+                    let errorDescription = errorDictionary["description"] as? String {
+                    
+                    print(errorDescription)
+                    
+                    if self.tries == 6 {
+                        print("invalidating timer")
+                        timer.invalidate()
+                    }
+                    
+                } else {
+                    print("didnt receive expected response type")
+                }
+                
+                self.tries += 1
         }
         
     }
